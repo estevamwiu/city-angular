de721eb977a012c4f2a7cd629aa5f01cf7e2b5e4
William Estevam <estevamwilliam16@gmail.com>
2025-09-02 18:12:44 -0300

    :sparkles: feat: Create componente city

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CityComponent } from './city-component/city-component';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    CityComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
---
+++ src/app/city-component/city-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-city-component',
+  standalone: false,
+  templateUrl: './city-component.html',
+  styleUrl: './city-component.css'
+})
+export class CityComponent {
+
+}
